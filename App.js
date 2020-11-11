d2917273eabb4052a9d909393ff07b7c8a8bdd1f
deu04287 <48381510+deu04287@users.noreply.github.com>
2020-11-12 05:20:17 +0900

    what?

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View style = {styles.defaultCss}>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  defaultCss: {
+    flex : 1,
+    justifyContent : "center",
+    alignItems : "center",
+  }
+
+});
+
+export default App;
